c6cb71923a2bf5734154c9a9004f2295ea5f7c75
hsran <hsran@cognitivescale.com>
2018-11-23 17:16:04 +0530

    booking

---
+++ tutor-app/src/main/java/com/hackoverflow/tutorapp/model/BookingDetails.java
@@ -0,0 +1,13 @@
+package com.hackoverflow.tutorapp.model;
+
+import java.sql.Timestamp;
+import java.time.LocalDate;
+
+public class BookingDetails {
+    public Timestamp timeofbooking;
+    public Long tutorId;
+    public Long userId;
+    public Timestamp classTime;
+    public Long Id;
+
+}
---
+++ tutor-app/src/main/java/com/hackoverflow/tutorapp/model/Bookings.java
@@ -0,0 +1,8 @@
+package com.hackoverflow.tutorapp.model;
+
+public class Bookings {
+    public String bookingId;
+    public BookingDetails bookingDetails;
+
+
+}
